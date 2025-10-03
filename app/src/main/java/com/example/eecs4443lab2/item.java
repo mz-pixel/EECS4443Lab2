272a5db0ce44e1fb615662ca6883714cbcf6dc8f
Muhammad Zamin <63674082+mz-pixel@users.noreply.github.com>
2025-10-03 10:21:41 -0400

    first commit

---
+++ app/src/main/java/com/example/eecs4443lab2/item.java
@@ -0,0 +1,15 @@
+package com.example.eecs4443lab2;
+
+public class item {
+    String title;
+    int imageResId;
+    String description;
+
+    public item(String title, int imageResId, String description) {
+        this.title = title;
+        this.imageResId = imageResId;
+        this.description = description;
+    }
+
+    
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "EECS4443Lab2"
+include(":app")
+ 
