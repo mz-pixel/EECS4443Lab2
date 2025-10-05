d5f803b72d240876a89da1efca26563d8c684541
Muhammad Zamin <63674082+mz-pixel@users.noreply.github.com>
2025-10-05 07:00:53 -0400

    created new branch

--- app/src/main/java/com/example/eecs4443lab2/Item.java
+++ app/src/main/java/com/example/eecs4443lab2/Item.java
@@ -1,11 +1,15 @@
 package com.example.eecs4443lab2;
 
-public class item {
+public class Item {
+
+//    class variables
     String title;
     int imageResId;
     String description;
 
-    public item(String title, int imageResId, String description) {
+    // used android studio's generator to generate the constructor and get/set methods
+
+    public Item(String title, int imageResId, String description) {
         this.title = title;
         this.imageResId = imageResId;
         this.description = description;
